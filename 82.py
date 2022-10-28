fb03ba320f65e1aecafefa8d7e8b8ad48ede64df
Paul Kern <pkern@laas.fr>
2022-10-28 14:00:06 +0200

    82 done

---
+++ 82.py
@@ -0,0 +1,60 @@
+matrix = []
+
+with open('82.txt', 'r') as fi:
+    for line in fi:
+        matrix.append(list(map(int, line.split(','))))
+
+HEIGHT = len(matrix)
+WIDTH = len(matrix[0])
+
+graph = dict()
+weights = dict()
+for y in range(HEIGHT):
+    for x in range(WIDTH):
+        weights[(x, y)] = matrix[y][x]
+        neighbors = []
+        if y > 0:
+            neighbors.append((x, y - 1))
+        if y < HEIGHT - 1:
+            neighbors.append((x, y + 1))
+        if x < WIDTH - 1:
+            neighbors.append((x + 1, y))
+        graph[(x, y)] = neighbors
+
+def dijkstra(SOURCE):
+    print(SOURCE)
+    dist = dict()
+    prev = dict()
+    Q = list()
+    for v in graph:
+        dist[v] = 100000000 * WIDTH * HEIGHT
+        prev[v] = None
+        Q.append(v)
+
+    dist[SOURCE] = 0
+
+    while len(Q) > 0:
+        u = min(Q, key=lambda v:dist[v])
+        Q.remove(u)
+
+        for v in graph[u]:
+            alt = dist[u] + weights[v]
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u
+
+    min_cost = 100000000 * WIDTH * HEIGHT
+
+    for target_y in range(HEIGHT):
+        TARGET = (WIDTH - 1, target_y)
+        total_cost = 0
+        u = TARGET
+        while u is not None:
+            total_cost += weights[u]
+            u = prev[u]
+        if total_cost < min_cost:
+            min_cost = total_cost
+
+    return min_cost
+
+print(min(dijkstra((0, source_y)) for source_y in range(HEIGHT)))
